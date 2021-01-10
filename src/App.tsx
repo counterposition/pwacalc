6138b424bd3eda238edf722f23a6d87ad2b1e728
Harish Kukreja <harish.kukreja@gmail.com>
2021-01-10 15:02:09 -0500

    Make calculator interactive

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,48 @@
+import { useReducer } from 'react';
+import { reducer, initialState, Action, digit, CalculatorState } from './action';
 import './App.css';
 
-namespace Action {
-  interface IAction {}
-
-  class ActionWithValue<T> implements IAction {
-    constructor(public readonly value: T) {}
-  }
-
-  type digit = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
-  class Operand extends ActionWithValue<digit> {
-    constructor(readonly value: digit) {
-      super(value);
-    }
-  }
-
-  class Action implements IAction {
-    private constructor(private readonly s: string) {}
-
-    public toString() { return this.s; }
-
-    public static Add = new Action('add');
-    public static Subtract = new Action('subtract');
-    public static Multiply = new Action('multiply');
-    public static Divide = new Action('divide');
-    public static Evaluate = new Action('evaluate');
-
-    public static Operand(value: digit): Operand {
-      return new Operand(value);
-    }
-  }
-
-  // FIX THIS: state should not be of type any
-  function reducer(state: any, action: IAction) {
-    if (action instanceof Action) {
-        // do stuff
-    } else if (action instanceof Operand) {
-        const v = action.value;
-        // do some stuff with v
-    } else {
-      throw new Error('action is neither an Action nor an Operand')
-    }
-  }
-}
 
 const button_order = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
 
+function cancelLabel({register, operand, operator}: CalculatorState): string {
+  if (register.isEmpty() && !operand.isEmpty()) return 'C';
+  if (operand.isEmpty()) return 'AC';
+  else return 'C';
+}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <>
       { /* placeholder until we implement interactivity */}
-      <div id="output">0</div>
+      <div id="output">
+        { state.operand.isEmpty() && !state.register.isEmpty() ? state.register.toString() : state.operand.toString() }
+      </div>
       <div id="input">
         <div id="operands">
-          <div id="cancel">AC</div>
-          <div id="negate">~</div>
-          <div id="percent">%</div>
+          <div id="cancel" onClick={() => dispatch(Action.Cancel) }>{ cancelLabel(state) }</div>
+          <div id="negate" title="negate" onClick={() => dispatch(Action.Negate) }>~</div>
+          <div id="percent" onClick={() => dispatch(Action.Percent) }>%</div>
 
           {
-            button_order.map(n => <div className="number" key={n}>{n}</div>)
+            button_order.map(n => <div className="number"
+                                       key={n}
+                                       onClick={() => dispatch(Action.Operand(n as digit))}>
+                                         {n}
+                                  </div>)
           }
 
-          <div id="dot">.</div>
+          <div id="dot" onClick={() => dispatch(Action.Operand('.'))}>.</div>
         </div>
 
         <div id="operators">
-          <div id="divide">÷</div>
-          <div id="multiply">×</div>
-          <div id="subtract">−</div>
-          <div id="add">+</div>
-          <div id="equals">=</div>
+          <div id="divide" onClick={() => dispatch(Action.Divide)}>÷</div>
+          <div id="multiply" onClick={() => dispatch(Action.Multiply)}>×</div>
+          <div id="subtract" onClick={() => dispatch(Action.Subtract)}>−</div>
+          <div id="add" onClick={() => dispatch(Action.Add)}>+</div>
+          <div id="equals" onClick={() => dispatch(Action.Evaluate)}>=</div>
         </div>
       </div>
     </>
